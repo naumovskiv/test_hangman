9b51ee29198fd764590078bad0f6005d95369b7d
naumovskiv <110610516+naumovskiv@users.noreply.github.com>
2023-04-23 18:15:22 -0400

    Add files via upload
    add py file with simple hangman image function for game

---
+++ hangmanimage.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 23 17:37:19 2023
+
+@author: viktor
+"""
+
+def hmimage(chances=0):
+    if chances==7:
+        print(" _______")
+        print(" |     |")
+        print(" |      ")
+        print(" |      ")
+        print(" |      ")
+        print(" |      ")
+        print("_|__    ")
+    elif chances==6:
+        print(" _______")
+        print(" |     |")
+        print(" |     O ")
+        print(" |      ")
+        print(" |      ")
+        print(" |      ")
+        print("_|__    ")
+    elif chances==5:
+        print(" _______")
+        print(" |     |")
+        print(" |     O ")
+        print(" |     | ")
+        print(" |      ")
+        print(" |      ")
+        print("_|__    ")
+    elif chances==4:
+        print(" _______")
+        print(" |     |")
+        print(" |     O ")
+        print(" |     | ")
+        print(" |     | ")
+        print(" |      ")
+        print("_|__    ")
+    elif chances==3:
+        print(" _______")
+        print(" |     |")
+        print(" |     O ")
+        print(" |    /| ")
+        print(" |     | ")
+        print(" |      ")
+        print("_|__    ")
+    elif chances==2:
+        print(" _______")
+        print(" |     |")
+        print(" |     O ")
+        print(" |    /|\ ")
+        print(" |     | ")
+        print(" |      ")
+        print("_|__    ")
+    elif chances==1:
+        print(" _______")
+        print(" |     |")
+        print(" |     O ")
+        print(" |    /|\ ")
+        print(" |     | ")
+        print(" |    /  ")
+        print("_|__    ")
+    elif chances==0:
+        print(" _______")
+        print(" |     |")
+        print(" |     O ")
+        print(" |    /|\ ")
+        print(" |     | ")
+        print(" |    / \ ")
+        print("_|__    ")
